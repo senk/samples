ad6fc955224c7b1968dfd3f401dfa77796a69648
AllaeddineEL <a.elareed@gmail.com>
2021-09-22 10:41:52 +0200

    Update server.js

--- nodejs/npm/server.js
+++ nodejs/npm/server.js
@@ -10,7 +10,7 @@ app.get('/', (request, response) => {
     <title>Powered By Paketo Buildpacks</title>
   </head>
   <body>
-    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://paketo.io/images/paketo-logo-full-color.png"></img>
+    <img style="display: block; margin-left: auto; margin-right: auto; width: 50%;" src="https://docs.vmware.com/en/VMware-Tanzu-Kubernetes-Grid/images/GUID-8546DDD9-718A-42F7-9EDB-0BCC3A316BB6-low.png"></img>
   </body>
 </html>`);
 });
